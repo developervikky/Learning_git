51607ceb06804f5500e8d5f5e21bd02909c78cd0
Vikky <developer.vikky@gmail.com>
2022-01-17 21:11:37 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function add(a, b){
-	return a+b;
-}
+const add = require('./add');
+const minus = require('./minus');
+const devide = require('./devide');
+const multiply = require('./multiply');
 
 console.log(add(10,5));
+console.log(minus(10,5));
+console.log(devide(10,5));
+console.log(multiply(10,5));
